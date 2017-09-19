c625321e96a26d82dcd6d04e667aa99c67f5e4cc
Brett Adcock <brettadcock@ruelala.com>
2017-09-19 11:34:04 -0400

    added a mutex for writing to the file

--- logic/groceries.go
+++ logic/groceries.go
@@ -3,15 +3,20 @@ package logic
 import (
 	"io/ioutil"
 	"os"
+	"sync"
 )
 
 type GroceryList struct {
 	Title string
 	Body  []byte
+	mu    sync.Mutex
 }
 
 func (gl GroceryList) Save() error {
-	return ioutil.WriteFile("./resources/grocery-list.txt", gl.Body, os.ModeDevice)
+	gl.mu.Lock()
+	err := ioutil.WriteFile("./resources/grocery-list.txt", gl.Body, os.ModeDevice)
+	gl.mu.Unlock()
+	return err
 }
 
 func NewGroceryList() GroceryList {
