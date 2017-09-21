375a9d50e3f3b000880c2c731691cfaac4e41aac
Brett Adcock <brettadcock@ruelala.com>
2017-09-21 13:51:55 -0400

    removed commented code and logged port number

--- service/service.go
+++ service/service.go
@@ -17,7 +17,6 @@ func init() {
 }
 
 func ListenAndServe() {
-	//disInfo := DisplayInfo{logicDataAccess: logic.New(), gl: logic.NewGroceryList()}
 	router := mux.NewRouter()
 
 	router.HandleFunc("/", Index)
@@ -26,6 +25,6 @@ func ListenAndServe() {
 	router.HandleFunc("/edit/grocery-list", EditGroceries)
 	router.HandleFunc("/save/grocery-list", SaveGroceries)
 
-	log.Println("Listening...")
+	log.Println("Listening...(port: 9000)")
 	http.ListenAndServe(bindTo, router)
 }
